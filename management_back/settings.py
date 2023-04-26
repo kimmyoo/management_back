3c60e73478fe8e931682b9ebf7108927a5dc9469
kimmyoo <kimmyoo@gmail.com>
2023-04-26 00:30:29 -0400

    changed allowed host for deploymentj

--- management_back/settings.py
+++ management_back/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["abcschoolmanagement.pythonanywhere.com"]
 
 # django-cors-header setting allowed origin
 CORS_ALLOWED_ORIGINS = [
