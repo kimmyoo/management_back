bd13986cc2aa8c930976d102bf601a0e45e66a77
kimmyoo <kimmyoo@gmail.com>
2023-04-28 09:29:38 -0400

    Lax not lax

--- management_back/settings.py
+++ management_back/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["abcschoolmanagement.pythonanywhere.com"]
+ALLOWED_HOSTS = ["abcschoolmanagement.pythonanywhere.com", "127.0.0.1"]
 
 # django-cors-header setting allowed origin
 CORS_ALLOWED_ORIGINS = [
@@ -145,7 +145,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -154,7 +154,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
 
-# for python anywhere deployment
+# for python anywhere deployment static dirs
+# also see urls.py
+STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     #'/var/www/static/',
